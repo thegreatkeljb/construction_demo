6a0275eeb3ffd65d75d9a9c02815ec6a1ce812aa
Michael <shizune.mjbg@gmail.com>
2026-09-12 16:14:32 +0800

    Build: Added and setup Tailwind

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react()
+    react(),
+    tailwindcss(),
   ],
   server: {
     historyApiFallback: true,
